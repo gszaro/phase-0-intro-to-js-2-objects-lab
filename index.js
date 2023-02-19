29aa183d1e5eb2be937145341be5c9198ef30cb2
Gregory Szaro <gregoryszaro@gmail.com>
2023-02-18 22:32:04 -0500

    completes obj lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+    name: "Gregory",
+    streetAddress: "123 Fake St."
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+        const newEmp = { ...employee };
+        newEmp[key] = value;
+        return newEmp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+        return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmp = { ...employee };
+    delete newEmp[key];
+    return newEmp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
